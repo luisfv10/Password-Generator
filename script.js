7929223b30e9b965ebd17231156ee52c91a92f81
luisfv10 <lfvp@cin.ufpe.br>
2023-06-25 19:56:30 -0300

    Finishing the generator

---
+++ script.js
@@ -0,0 +1,30 @@
+let passwordLenght = 16
+const inputEl = document.querySelector("#password")
+function generatePassword() {
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789?!@&*()[]"
+
+  let password = ""
+
+  for (let i = 0; i < passwordLenght; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length)
+    password += chars.substring(randomNumber, randomNumber + 1)
+  }
+
+  inputEl.value = password
+}
+
+function copy() {
+  navigator.clipboard.writeText(inputEl.value)
+}
+
+const passwordLenghtEl = document.querySelector("#password-lenght")
+passwordLenghtEl.addEventListener("input", function () {
+  passwordLenght = passwordLenghtEl.value
+  generatePassword()
+})
+
+const copyButtonEl = document.querySelector("#copy")
+copyButtonEl.addEventListener("click", copy)
+
+generatePassword()
